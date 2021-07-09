144805ed121604675aa1d6fa5bdce67676cada37
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-07-09 22:21:23 +0900

    Update doc

--- src/doc.tsx
+++ src/doc.tsx
@@ -1,19 +1,48 @@
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 type BlockId = string;
 
 type Block = TextBlock;
 
+export class Cursor {
+  public doc: Doc;
+
+  constructor(cursor: {
+    id?: string;
+    blockId?: BlockId | null;
+    range?: {
+      anchor: number;
+      focus: number;
+    } | null;
+  } = {}) {
+    this.id = cursor.id || uuid();
+    this.blockId = cursor.blockId || null;
+    this.range = cursor.range || null;
+  }
+
+  public toJSON() {
+    return {
+      id: this.id,
+      range: this.range,
+    }
+  }
+}
+
 export class Doc {
   public id: string;
+
+  private cursor: Cursor;
 
   private blocks: Block[];
 
   constructor(doc: {
     id?: string;
+    cursor?: Cursor[];
     blocks?: Block[];
   } = {}) {
     this.id = doc.id || uuid();
+    this.cursor = doc.cursor ? new Cursor(doc.cursor) : new Cursor();
+    this.cursor.doc = this;
     this.blocks = doc.blocks ? doc.blocks.map(b => new TextBlock(b)) : [];
   }
 
@@ -25,6 +54,7 @@ export class Doc {
   public toJSON() {
     return {
       id: this.id,
+      cursor: this.cursor.toJSON(),
       blocks: this.blocks.map((b) => {
         return b.toJSON();
       }),
@@ -52,6 +82,7 @@ export class TextBlock {
   }
 
   public append(block: Block) {
+    block.doc = this.doc;
     this.children.push(block);
   }
 
