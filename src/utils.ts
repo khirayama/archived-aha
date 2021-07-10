408316198529e27088cddca45046b33d880f06eb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-07-11 07:12:17 +0900

    Fix bugs in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,23 @@
 import { Children } from 'react';
-import { BlockId, Block, DocumentBlock } from './doc';
+import { BlockId, ItemBlockType, DocumentBlockType, ItemBlock } from './doc';
 
 export const utils = {
-  findDownerBlock: (currentBlockId: BlockId, doc: DocumentBlock): Block | null => {
+  findDownerBlock: (currentBlockId: BlockId, doc: DocumentBlockType): ItemBlockType | null => {
     const currentBlock = doc.find(currentBlockId);
     if (currentBlock.children.length) {
       return currentBlock.children[0];
     } else if (currentBlock.next) {
       return currentBlock.next;
-    } else if (currentBlock.doc !== currentBlock.parent && currentBlock.parent && currentBlock.parent.next) {
-      return (currentBlock.parent as Block).next;
+    } else {
+      let b = currentBlock;
+      while (b.parent !== null && b.parent.isItemBlock() && b.parent.next === null) {
+        b = b.parent;
+      }
+      return b.parent.isItemBlock() ? b.parent.next : null;
     }
-    return null;
   },
-  findUpperBlock: (currentBlockId: BlockId, doc: DocumentBlock): Block | null => {
+
+  findUpperBlock: (currentBlockId: BlockId, doc: DocumentBlockType): ItemBlockType | null => {
     const currentBlock = doc.find(currentBlockId);
     if (currentBlock.prev && currentBlock.prev.children.length) {
       let children = currentBlock.prev.children;
