b82f4ac7ddd6a920d64e416f9d1b502cba43741c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-09-23 13:49:08 +0900

    Rename sample to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,197 +1,102 @@
 import * as React from 'react';
 
-import { DocumentBlock, TextBlock } from '../doc';
+import { DocumentBlock, TextBlock, Cursor } from '../doc';
 import { keyCodes } from '../keyCodes';
-import { utils } from '../utils';
+
+function projectSelectionToCursor(selection: Selection, cursor: Cursor): void {
+  cursor.anchorId = selection.anchorNode?.parentNode.dataset.id || null;
+  cursor.anchorOffset = selection.anchorOffset || 0;
+  cursor.focusId = selection.focusNode?.parentNode.dataset.id || null;
+  cursor.focusOffset = selection.focusOffset || 0;
+  cursor.isCollapsed = selection.isCollapsed;
+  doc.dispatch();
+}
+
+function onSelectionChange() {
+  const selection = document.getSelection();
+  if (doc && selection.anchorNode?.parentNode.dataset.id) {
+    projectSelectionToCursor(selection, doc.cursor);
+  }
+}
+
+function Block(props) {
+  const block = props.block;
+  return (
+    <>
+      <div data-id={block.id}>{block.text}</div>
+      <div style={{paddingLeft: '1rem'}}>{block.children.map((b) => <Block key={b.id} block={b} />)}</div>
+    </>
+  );
+}
+
+function CursorViewer(props) {
+  const cursor = props.cursor;
+  return (
+    <div style={{whiteSpace: 'pre'}}>{`
+anchorId    : ${cursor.anchorId}
+anchorOffset: ${cursor.anchorOffset}
+focusId     : ${cursor.focusId}
+focusOffset : ${cursor.focusOffset}
+isCollapsed : ${cursor.isCollapsed}`}
+    </div>
+  );
+}
 
 let doc = null;
 
-function Inline({ block, cursor }) {
-  const textStyle = {
-    background: block.id === cursor.blockId && cursor.range !== null ? 'rgba(0, 255, 0, 0.05)' : 'transparent',
-  };
-
-  if (cursor.blockId !== block.id || cursor.range === null) {
-    return <span style={textStyle}>{block.text}</span>;
-  } else if (cursor.range.anchor !== cursor.range.focus) {
-    return (
-      <span style={textStyle}>
-        <span>{block.text.slice(0, Math.min(cursor.range.anchor, cursor.range.focus))}</span>
-        <span style={textStyle}>{block.text.slice(Math.min(cursor.range.anchor, cursor.range.focus), Math.max(cursor.range.anchor, cursor.range.focus))}</span>
-        <span>{block.text.slice(Math.max(cursor.range.anchor, cursor.range.focus), block.text.length)}</span>
-      </span>
-    );
-  }
-  return (
-    <span style={textStyle}>
-      {block.text.slice(0, cursor.range.anchor)}|{block.text.slice(cursor.range.anchor, block.text.length)}
-    </span>
-  );
-}
-
-function Block({block, cursor}) {
-  const style = {
-    display: 'block',
-    verticalAlign: 'top',
-    whiteSpace: 'pre',
-    background: block.id === cursor.blockId && cursor.range === null ? 'green' : 'rgba(100, 0, 0, 0.05)',
-  };
-
-
-  return (
-    <span style={style}>
-      <span>
-        {`{
-  id: "${block.id}",
-  type: "${block.type}",
-  text: "`}</span><Inline block={block} cursor={cursor} /><span>{`",
-  children: [`}</span>
-      <span style={{paddingLeft: '1em', display: 'block'}}>
-        {block.children.map((b) => {
-          return <Block key={b.id} block={b} cursor={cursor} />
-        })}
-      </span>
-      <span>{`  ]
-}`}
-      </span>
-    </span>
-  );
-}
-
-export default function IndexPage(props) {
+export default function SamplePage(props) {
   if (doc === null) {
     doc = new DocumentBlock(props.doc);
   }
 
+  React.useEffect(() => {
+    document.addEventListener('selectionchange', onSelectionChange);
+    doc.addChangeListener(() => {
+      setDocJSON(doc.toJSON());
+    });
+
+    return () => {
+      document.removeEventListener('selectionchange', onSelectionChange);
+      doc.removeChangeListener();
+    };
+  });
+
   const [docJSON, setDocJSON] = React.useState(doc.toJSON());
-  const [value, setValue] = React.useState('');
-  const [keyCode, setKeyCode] = React.useState(null);
-
-  const onChange = React.useCallback((event: React.FormEvent<HTMLInputElement>) => {
-    const val = event.currentTarget.value;
-    console.log(val);
-    // setValue(val);
-  }, []);
-  const onKeyDown = React.useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
-    const keyCode = event.keyCode;
-
-    if (keyCode === keyCodes.DOWN) {
-      if (doc.cursor.blockId === null) {
-        const firstId = doc.children[0].id;
-        const firstBlock = doc.find(firstId);
-        doc.cursor.blockId = firstBlock.id
-        if (doc.cursor.range !== null && firstBlock.hasText()) {
-          doc.cursor.range = {
-            anchor: firstBlock.text.length,
-            focus: firstBlock.text.length,
-          };
-        }
-      } else {
-        const downer = utils.findDownerBlock(doc.cursor.blockId, doc);
-        if (downer) {
-          doc.cursor.blockId = downer.id;
-          if (doc.cursor.range !== null && downer.hasText()) {
-            doc.cursor.range = {
-              anchor: downer.text.length,
-              focus: downer.text.length,
-            };
-          }
-        }
+  const onKeyDown = (event) => {
+    switch (event.keyCode) {
+      case keyCodes.LEFT: {
+        break;
       }
-    } else if (keyCode === keyCodes.UP) {
-      if (doc.cursor.blockId === null) {
-        const lastId = doc.children[doc.children.length - 1].id;
-        const lastBlock = doc.find(lastId);
-        doc.cursor.blockId = lastBlock.id
-        if (doc.cursor.range !== null && lastBlock.hasText()) {
-          doc.cursor.range = {
-            anchor: lastBlock.text.length,
-            focus: lastBlock.text.length,
-          };
+      case keyCodes.UP: {
+        break;
+      }
+      case keyCodes.RIGHT: {
+        break;
+      }
+      case keyCodes.DOWN: {
+        break;
+      }
+      default: {
+        if (event.metaKey /* TODO: Support Mac/Win/Linux */) {
+        } else {
+          event.preventDefault();
         }
-      } else {
-        const upper = utils.findUpperBlock(doc.cursor.blockId, doc);
-        if (upper) {
-          doc.cursor.blockId = upper.id;
-          if (doc.cursor.range !== null && upper.hasText()) {
-            doc.cursor.range = {
-              anchor: upper.text.length,
-              focus: upper.text.length,
-            };
-          }
-        }
-      }
-    } else if (keyCode === keyCodes.LEFT) {
-      if (doc.cursor.range === null) {
-        const block = doc.find(doc.cursor.blockId);
-        if (block.parent && !block.parent.isDocumentBlock()) {
-          doc.cursor.blockId = block.parent.id;
-        }
-      } else if (doc.cursor.range.anchor === doc.cursor.range.focus) {
-        doc.cursor.range.anchor = Math.max(doc.cursor.range.anchor - 1, 0);
-        doc.cursor.range.focus = Math.max(doc.cursor.range.focus - 1, 0);
-      }
-    } else if (keyCode === keyCodes.RIGHT) {
-      const block = doc.find(doc.cursor.blockId);
-      if (doc.cursor.range === null) {
-        if (block.children.length) {
-          doc.cursor.blockId = block.children[0].id;
-        }
-      } else if (doc.cursor.range.anchor === doc.cursor.range.focus) {
-        doc.cursor.range.anchor = Math.min(doc.cursor.range.anchor + 1, block.text.length);
-        doc.cursor.range.focus = Math.min(doc.cursor.range.focus + 1, block.text.length);
-      }
-    } else if (keyCode === keyCodes.ENTER) {
-      if (doc.cursor.blockId) {
-        const block = doc.find(doc.cursor.blockId);
-        doc.cursor.range = {
-          anchor: block.text.length,
-          focus: block.text.length,
-        };
-      }
-    } else if (keyCode === keyCodes.ESC) {
-      if (doc.cursor.blockId) {
-        doc.cursor.range = null;
-      }
-    } else {
-      const max = Math.max(doc.cursor.range.anchor, doc.cursor.range.focus);
-      const min = Math.min(doc.cursor.range.anchor, doc.cursor.range.focus);
-      if (doc.cursor.range.anchor === doc.cursor.range.focus) {
-        const val = event.currentTarget.value;
-        const block = doc.find(doc.cursor.blockId);
-        let tmp = block.text.split('');
-        tmp.splice(min, max - min, val)
-        block.text = tmp.join('');
       }
     }
-    setKeyCode(keyCode);
-    setDocJSON(doc.toJSON());
-  }, []);
-
-  const cursor = docJSON.cursor;
+  };
 
   return (
     <div>
-      <input type="text" value={value} onChange={onChange} onKeyDown={onKeyDown} autoFocus />
-      <div>
-        <span>keyCode: {keyCode}</span>
+      <CursorViewer cursor={doc.cursor} />
+      <textarea />
+      <div
+        onKeyDown={onKeyDown}
+        onMouseUp={() => document.querySelector('textarea').focus()}
+      >
+        {docJSON.children.map((block) => {
+          return <Block key={block.id} block={block} />;
+        })}
       </div>
-      <h3>Cursor</h3>
-      <div style={{whiteSpace: 'pre'}}>
-        {`{
-  id: "${cursor.id}",
-  blockId: "${cursor.blockId}",
-  range: ${cursor.range === null ? 'null' : (`
-    anchor: ${cursor.range.anchor},
-    focus: ${cursor.range.focus},
-  `)}
-}`
-        }
-      </div>
-      <h3>Blocks</h3>
-      {docJSON.children.map((b) => {
-        return <Block key={b.id} block={b} cursor={cursor} />
-      })}
     </div>
   );
 }
