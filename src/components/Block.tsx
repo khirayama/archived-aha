2898ee1d0e243df980fe16c4dbd07501dbd25ad9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-09-25 17:24:12 +0900

    Apply style to Block

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,9 +1,12 @@
+import classNames from 'classnames';
+
 import styles from './Block.module.scss';
 
 export function Block(props) {
   const block = props.block;
   const cursor = props.cursor;
-  const isFocusing = (cursor.anchorId === block.id || cursor.focusId === block.id) && (cursor.anchorOffset !== null || cursor.focusOffset !== null);
+  const isSelected = (cursor.anchorId === block.id || cursor.focusId === block.id) && (cursor.anchorOffset === null || cursor.focusOffset === null);
+  const isFocused = (cursor.anchorId === block.id || cursor.focusId === block.id) && (cursor.anchorOffset !== null || cursor.focusOffset !== null);
 
   const textArr = Array.from(block.text);
   const p1 = Math.min(cursor.anchorOffset, cursor.focusOffset);
@@ -11,11 +14,16 @@ export function Block(props) {
   const t1 = textArr.slice(0, p1);
   const t2 = textArr.slice(p1, p2);
   const t3 = textArr.slice(p2, textArr.length);
+
   return (
     <>
-      <div className={styles.block}>
-        <div data-id={block.id} style={{background: isFocusing ? 'red' : ''}}>{block.text}</div>
-        {isFocusing ? <div>{t1}|{t2}|{t3}</div> : null }
+      <div className={classNames(styles['block'], { [styles['is-selected']]: isSelected })}>
+        <div data-id={block.id}>{block.text}</div>
+        {isFocused ? <div className={styles['shadow']}>
+          <span>{t1}</span>
+          <span>{t2}</span>
+          <span>{t3}</span>
+        </div> : null }
       </div>
       <div style={{paddingLeft: '1rem'}}>{block.children.map((b) => <Block key={b.id} block={b} cursor={cursor} />)}</div>
     </>
