98610255ad6d37ea5fe96e6c2a836358564ff5b2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-03 07:23:33 +0900

    Update prosemirror sample

--- src/pages/prose-mirror.tsx
+++ src/pages/prose-mirror.tsx
@@ -3,12 +3,14 @@ import { EditorView } from "prosemirror-view";
 import { schema } from "prosemirror-schema-basic";
 import { undo, redo, history } from "prosemirror-history"
 import { keymap } from "prosemirror-keymap"
+import { baseKeymap } from "prosemirror-commands"
 import React, { useEffect, useRef } from "react";
 
 /*
  * - [Lightweight React integration example - Show - discuss.ProseMirror](https://discuss.prosemirror.net/t/lightweight-react-integration-example/2680)
  * - [ProseMirror Guide](https://prosemirror.net/docs/guide/)
  * - [ProseMirror Reference manual](https://prosemirror.net/docs/ref/#commands)
+ * - [remirror/remirror: ProseMirror toolkit for React 🎉](https://github.com/remirror/remirror)
  */
 
 function Editor(props) {
@@ -24,14 +26,16 @@ function Editor(props) {
           "Mod-z": undo,
           "Mod-y": redo,
         }),
+        keymap(baseKeymap),
       ]
     });
     const view = new EditorView(ref.current, {
       state,
       dispatchTransaction: (transaction) => {
+        console.log(transaction);
         console.log("Document size went from", transaction.before.content.size,
                     "to", transaction.doc.content.size)
-        let newState = view.state.apply(transaction)
+        const newState = view.state.apply(transaction)
         view.updateState(newState)
       }
     });
