4322af8bcafaaf0f58d47d6c85836aca38f2e99d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-02 17:40:22 +0900

    Apply styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import './reset.scss';
+import './theme.scss';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
