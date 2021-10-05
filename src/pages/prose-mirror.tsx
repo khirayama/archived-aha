ea60d732e576a2803aacb3778d068206250c4607
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-06 07:34:19 +0900

    Define own schema

--- src/pages/prose-mirror.tsx
+++ src/pages/prose-mirror.tsx
@@ -1,6 +1,6 @@
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
-import { schema } from "prosemirror-schema-basic";
+// import { schema } from "prosemirror-schema-basic";
 import { undo, redo, history } from "prosemirror-history"
 import { keymap } from "prosemirror-keymap"
 import { baseKeymap } from "prosemirror-commands"
@@ -14,70 +14,24 @@ import { Schema } from "prosemirror-model"
  * - [ProseMirror Reference manual](https://prosemirror.net/docs/ref/#commands)
  * - [remirror/remirror: ProseMirror toolkit for React 🎉](https://github.com/remirror/remirror)
  * - [vim keymap](https://codemirror.net/3/keymap/vim.js)
+ * - [ProseMirror dino example](https://prosemirror.net/examples/dino/)
  */
 
-const dinos = [
-  "brontosaurus",
-  "stegosaurus",
-  "triceratops",
-  "tyrannosaurus",
-  "pterodactyl",
-];
-
-const dinoNodeSpec = {
-  attrs: {
-    type: {
-      default: "brontosaurus",
+const schema = new Schema({
+  nodes: {
+    doc: {
+      content: "block+",
     },
-  },
-  inline: true,
-  group: "inline",
-  draggable: true,
-  toDOM: node => [
-    "img",
-    {
-      "dino-type": node.attrs.type,
-      src: "https://prosemirror.net/img/dino/" + node.attrs.type + ".png",
-      title: node.attrs.type,
-      class: "dinosaur",
-    }
-  ],
-  parseDOM: [
-    {
-      tag: "img[dino-type]",
-      getAttrs: dom => {
-        let type = dom.getAttribute("dino-type")
-        return dinos.indexOf(type) > -1 ? {type} : false
-      }
+    paragraph: {
+      group: "block",
+      content: "text*",
+      toDOM: (node) => {
+        return ["div", 0];
+      },
     },
-  ]
-}
-
-const dinoSchema = new Schema({
-  nodes: schema.spec.nodes.addBefore("image", "dino", dinoNodeSpec),
-  marks: schema.spec.marks
-})
-
-let dinoType = dinoSchema.nodes.dino
-
-function insertDino(type) {
-  return function(state, dispatch) {
-    console.log('Hello');
-    console.log(state);
-
-    let { $from } = state.selection;
-    let index = $from.index();
-
-    if (!$from.parent.canReplaceWith(index, index, dinoType)) {
-      // console.log('Return earler');
-      // return false
-    }
-    if (dispatch) {
-      dispatch(state.tr.replaceSelectionWith(dinoType.create({type})))
-    }
-    return true
+    text: {}
   }
-}
+});
 
 function Editor(props) {
   const ref = useRef();
@@ -90,7 +44,6 @@ function Editor(props) {
         keymap({
           "Mod-z": undo,
           "Mod-y": redo,
-          "Mod-k": insertDino('stegosaurus'),
         }),
         keymap(baseKeymap),
       ]
