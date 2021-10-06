020b82052606702b02db054d94f387ab7c67166e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-06 17:32:00 +0900

    Update prose mirror page

--- src/pages/prose-mirror.tsx
+++ src/pages/prose-mirror.tsx
@@ -1,9 +1,9 @@
-import { EditorState } from "prosemirror-state";
+import { EditorState, Selection, Plugin } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 // import { schema } from "prosemirror-schema-basic";
 import { undo, redo, history } from "prosemirror-history"
 import { keymap } from "prosemirror-keymap"
-import { baseKeymap } from "prosemirror-commands"
+import { baseKeymap, selectParentNode } from "prosemirror-commands"
 import React, { useEffect, useRef } from "react";
 
 import { Schema } from "prosemirror-model"
@@ -16,6 +16,54 @@ import { Schema } from "prosemirror-model"
  * - [vim keymap](https://codemirror.net/3/keymap/vim.js)
  * - [ProseMirror dino example](https://prosemirror.net/examples/dino/)
  */
+/*
+ * - Change order with drag and drop
+ * - Select mode and insert mode
+ * - Indent / Outdent
+ * - Popup window
+ */
+class SelectionSizeTooltip {
+  constructor(view) {
+    this.tooltip = document.createElement("div")
+    this.tooltip.className = "tooltip"
+    view.dom.parentNode.appendChild(this.tooltip)
+
+    this.update(view, null)
+  }
+
+  update(view, lastState) {
+    let state = view.state
+    // Don't do anything if the document/selection didn't change
+    if (lastState && lastState.doc.eq(state.doc) &&
+      lastState.selection.eq(state.selection)) return
+
+    // Hide the tooltip if the selection is empty
+    if (state.selection.empty) {
+      this.tooltip.style.display = "none"
+      return
+    }
+
+    // Otherwise, reposition it and update its content
+    this.tooltip.style.display = ""
+    let {from, to} = state.selection
+    // These are in screen coordinates
+    let start = view.coordsAtPos(from), end = view.coordsAtPos(to)
+    // The box in which the tooltip is positioned, to use as base
+    let box = this.tooltip.offsetParent.getBoundingClientRect()
+    // Find a center-ish x position from the selection endpoints (when
+    // crossing lines, end may be more to the left)
+    let left = Math.max((start.left + end.left) / 2, start.left + 3)
+    this.tooltip.style.left = (left - box.left) + "px"
+    this.tooltip.style.bottom = (box.bottom - start.top) + "px"
+    this.tooltip.textContent = to - from
+  }
+
+  destroy() { this.tooltip.remove() }
+}
+
+let selectionSizePlugin = new Plugin({
+  view(editorView) { return new SelectionSizeTooltip(editorView) }
+});
 
 const schema = new Schema({
   nodes: {
@@ -25,6 +73,7 @@ const schema = new Schema({
     paragraph: {
       group: "block",
       content: "text*",
+      draggable: true,
       toDOM: (node) => {
         return ["div", 0];
       },
@@ -44,8 +93,10 @@ function Editor(props) {
         keymap({
           "Mod-z": undo,
           "Mod-y": redo,
+          "Escape": selectParentNode,
         }),
         keymap(baseKeymap),
+        selectionSizePlugin,
       ]
     });
 
