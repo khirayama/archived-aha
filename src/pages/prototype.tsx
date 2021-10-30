c76ef504878ae3ee82d2f005d071414f9802c2be
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-31 07:45:20 +0900

    Remove schema from prototype page

--- src/pages/prototype.tsx
+++ src/pages/prototype.tsx
@@ -4,9 +4,9 @@ import { undo, redo, history } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
 import React, { useEffect, useRef } from 'react';
 
-import { Schema } from 'prosemirror-model';
-
 import { customKeymap } from './prototype-commands';
+import { schema } from './prototype-prototype-schema';
+
 import styles from './prototype.module.scss';
 
 /*
@@ -14,49 +14,6 @@ import styles from './prototype.module.scss';
  * - [ ] Copy / Past
  *   - https://prosemirror.net/docs/ref/#view.EditorProps.handlePaste
  */
-
-const schema = new Schema({
-  nodes: {
-    doc: {
-      content: 'block+',
-    },
-    paragraph: {
-      group: 'block',
-      content: 'text*',
-      // draggable: true,
-      attrs: {
-        indent: {
-          default: 0,
-        },
-      },
-      toDOM: (node) => {
-        return [
-          'div',
-          {
-            type: 'paragraph',
-            indent: node.attrs.indent,
-            class: styles[`indent-${node.attrs.indent}`],
-          },
-          0,
-        ];
-      },
-      parseDOM: [
-        {
-          tag: 'div',
-          attrs: {
-            type: 'paragraph',
-          },
-          getAttrs: (dom) => {
-            return {
-              indent: dom.indent,
-            };
-          },
-        },
-      ],
-    },
-    text: {},
-  },
-});
 
 let preventTabKeyPlugin = new Plugin({
   props: {
