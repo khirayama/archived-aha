1113bd38fe39eb25b8f5b61f42eae33537519c11
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-10-30 14:32:09 +0900

    Create schema for prototype

---
+++ src/pages/prototype-schema.ts
@@ -0,0 +1,46 @@
+import { Schema } from 'prosemirror-model';
+
+import styles from './prototype.module.scss';
+
+export const schema = new Schema({
+  nodes: {
+    doc: {
+      content: 'block+',
+    },
+    paragraph: {
+      group: 'block',
+      content: 'text*',
+      // draggable: true,
+      attrs: {
+        indent: {
+          default: 0,
+        },
+      },
+      toDOM: (node) => {
+        return [
+          'div',
+          {
+            type: 'paragraph',
+            indent: node.attrs.indent,
+            class: styles[`indent-${node.attrs.indent}`],
+          },
+          0,
+        ];
+      },
+      parseDOM: [
+        {
+          tag: 'div',
+          attrs: {
+            type: 'paragraph',
+          },
+          getAttrs: (dom) => {
+            return {
+              indent: dom.indent,
+            };
+          },
+        },
+      ],
+    },
+    text: {},
+  },
+});
