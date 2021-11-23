ed6172b3794b243b893d5b1c084bc5015b79c7b2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-11-23 10:51:39 +0900

    Update prototype

--- src/pages/prototype-based-basic.tsx
+++ src/pages/prototype-based-basic.tsx
@@ -7,10 +7,29 @@ import { EditorView } from 'prosemirror-view';
 import { schema } from 'prosemirror-schema-basic';
 import { undo, redo, history } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
-import { baseKeymap } from 'prosemirror-commands';
 import React, { useEffect, useRef } from 'react';
-import { exampleSetup } from 'prosemirror-example-setup';
-import { lift, joinUp, selectParentNode, wrapIn, setBlockType } from 'prosemirror-commands';
+import {
+  inputRules,
+  wrappingInputRule,
+  textblockTypeInputRule,
+  smartQuotes,
+  emDash,
+  ellipsis,
+  undoInputRule,
+} from 'prosemirror-inputrules';
+import {
+  baseKeymap,
+  wrapIn,
+  setBlockType,
+  chainCommands,
+  toggleMark,
+  exitCode,
+  joinUp,
+  joinDown,
+  lift,
+  selectParentNode,
+} from 'prosemirror-commands';
+import { wrapInList, splitListItem, liftListItem, sinkListItem } from 'prosemirror-schema-list';
 
 import { Schema } from 'prosemirror-model';
 
@@ -60,7 +79,7 @@ const mySchema = new Schema({
         ];
       },
     },
-    blockgroup: {
+    indentation: {
       group: 'block',
       content: 'block+',
       defining: true,
@@ -68,7 +87,7 @@ const mySchema = new Schema({
         {
           tag: 'div',
           attrs: {
-            type: 'blockgroup',
+            type: 'indentation',
           },
         },
       ],
@@ -76,8 +95,8 @@ const mySchema = new Schema({
         return [
           'div',
           {
-            type: 'blockgroup',
-            class: styles['blockgroup'],
+            type: 'indentation',
+            class: styles['indentation'],
           },
           0,
         ];
@@ -94,19 +113,15 @@ function Editor(props) {
     const state = EditorState.create({
       schema: mySchema,
       plugins: [
-        ...exampleSetup({ schema: mySchema }),
+        history(),
+        buildInputRules(mySchema),
+        keymap(buildKeymap(mySchema)),
+        keymap(baseKeymap),
         keymap({
-          'Ctrl-<': lift,
+          'Mod-z': undo,
+          'Mod-y': redo,
         }),
       ],
-      // plugins: [
-      //   history(),
-      //   keymap({
-      //     'Mod-z': undo,
-      //     'Mod-y': redo,
-      //   }),
-      //   keymap(baseKeymap),
-      // ],
     });
 
     const view = new EditorView(ref.current, {
@@ -132,4 +147,160 @@ export default function IndexPage() {
       <Editor />
     </div>
   );
+}
+
+/* prosemirror-example-setup keymap */
+const mac = typeof navigator != 'undefined' ? /Mac/.test(navigator.platform) : false;
+
+// :: (Schema, ?Object) → Object
+// Inspect the given schema looking for marks and nodes from the
+// basic schema, and if found, add key bindings related to them.
+// This will add:
+//
+// * **Mod-b** for toggling [strong](#schema-basic.StrongMark)
+// * **Mod-i** for toggling [emphasis](#schema-basic.EmMark)
+// * **Mod-`** for toggling [code font](#schema-basic.CodeMark)
+// * **Ctrl-Shift-0** for making the current textblock a paragraph
+// * **Ctrl-Shift-1** to **Ctrl-Shift-Digit6** for making the current
+//   textblock a heading of the corresponding level
+// * **Ctrl-Shift-Backslash** to make the current textblock a code block
+// * **Ctrl-Shift-8** to wrap the selection in an ordered list
+// * **Ctrl-Shift-9** to wrap the selection in a bullet list
+// * **Ctrl->** to wrap the selection in a block quote
+// * **Enter** to split a non-empty textblock in a list item while at
+//   the same time splitting the list item
+// * **Mod-Enter** to insert a hard break
+// * **Mod-_** to insert a horizontal rule
+// * **Backspace** to undo an input rule
+// * **Alt-ArrowUp** to `joinUp`
+// * **Alt-ArrowDown** to `joinDown`
+// * **Mod-BracketLeft** to `lift`
+// * **Escape** to `selectParentNode`
+//
+// You can suppress or map these bindings by passing a `mapKeys`
+// argument, which maps key names (say `"Mod-B"` to either `false`, to
+// remove the binding, or a new key name string.
+export function buildKeymap(schema, mapKeys) {
+  let keys = {},
+    type;
+  function bind(key, cmd) {
+    if (mapKeys) {
+      let mapped = mapKeys[key];
+      if (mapped === false) return;
+      if (mapped) key = mapped;
+    }
+    keys[key] = cmd;
+  }
+
+  bind('Mod-z', undo);
+  bind('Shift-Mod-z', redo);
+  bind('Backspace', undoInputRule);
+  if (!mac) bind('Mod-y', redo);
+
+  bind('Alt-ArrowUp', joinUp);
+  bind('Alt-ArrowDown', joinDown);
+  bind('Mod-BracketLeft', lift);
+  bind('Escape', selectParentNode);
+
+  if ((type = schema.marks.strong)) {
+    bind('Mod-b', toggleMark(type));
+    bind('Mod-B', toggleMark(type));
+  }
+  if ((type = schema.marks.em)) {
+    bind('Mod-i', toggleMark(type));
+    bind('Mod-I', toggleMark(type));
+  }
+  if ((type = schema.marks.code)) bind('Mod-`', toggleMark(type));
+
+  if ((type = schema.nodes.bullet_list)) bind('Shift-Ctrl-8', wrapInList(type));
+  if ((type = schema.nodes.ordered_list)) bind('Shift-Ctrl-9', wrapInList(type));
+  if ((type = schema.nodes.indentation)) bind('Ctrl->', wrapIn(type));
+  if ((type = schema.nodes.hard_break)) {
+    let br = type,
+      cmd = chainCommands(exitCode, (state, dispatch) => {
+        dispatch(state.tr.replaceSelectionWith(br.create()).scrollIntoView());
+        return true;
+      });
+    bind('Mod-Enter', cmd);
+    bind('Shift-Enter', cmd);
+    if (mac) bind('Ctrl-Enter', cmd);
+  }
+  if ((type = schema.nodes.list_item)) {
+    bind('Enter', splitListItem(type));
+    bind('Mod-[', liftListItem(type));
+    bind('Mod-]', sinkListItem(type));
+  }
+  if ((type = schema.nodes.paragraph)) bind('Shift-Ctrl-0', setBlockType(type));
+  if ((type = schema.nodes.code_block)) bind('Shift-Ctrl-\\', setBlockType(type));
+  if ((type = schema.nodes.heading))
+    for (let i = 1; i <= 6; i++) bind('Shift-Ctrl-' + i, setBlockType(type, { level: i }));
+  if ((type = schema.nodes.horizontal_rule)) {
+    let hr = type;
+    bind('Mod-_', (state, dispatch) => {
+      dispatch(state.tr.replaceSelectionWith(hr.create()).scrollIntoView());
+      return true;
+    });
+  }
+
+  return keys;
+}
+
+/* prosemirror-example-setup inputrules */
+// : (NodeType) → InputRule
+// Given a indentation node type, returns an input rule that turns `"> "`
+// at the start of a textblock into a indentation.
+export function blockQuoteRule(nodeType) {
+  return wrappingInputRule(/^\s*>\s$/, nodeType);
+}
+
+// : (NodeType) → InputRule
+// Given a list node type, returns an input rule that turns a number
+// followed by a dot at the start of a textblock into an ordered list.
+export function orderedListRule(nodeType) {
+  return wrappingInputRule(
+    /^(\d+)\.\s$/,
+    nodeType,
+    (match) => ({ order: +match[1] }),
+    (match, node) => node.childCount + node.attrs.order == +match[1],
+  );
+}
+
+// : (NodeType) → InputRule
+// Given a list node type, returns an input rule that turns a bullet
+// (dash, plush, or asterisk) at the start of a textblock into a
+// bullet list.
+export function bulletListRule(nodeType) {
+  return wrappingInputRule(/^\s*([-+*])\s$/, nodeType);
+}
+
+// : (NodeType) → InputRule
+// Given a code block node type, returns an input rule that turns a
+// textblock starting with three backticks into a code block.
+export function codeBlockRule(nodeType) {
+  return textblockTypeInputRule(/^```$/, nodeType);
+}
+
+// : (NodeType, number) → InputRule
+// Given a node type and a maximum level, creates an input rule that
+// turns up to that number of `#` characters followed by a space at
+// the start of a textblock into a heading whose level corresponds to
+// the number of `#` signs.
+export function headingRule(nodeType, maxLevel) {
+  return textblockTypeInputRule(new RegExp('^(#{1,' + maxLevel + '})\\s$'), nodeType, (match) => ({
+    level: match[1].length,
+  }));
+}
+
+// : (Schema) → Plugin
+// A set of input rules for creating the basic block quotes, lists,
+// code blocks, and heading.
+export function buildInputRules(schema) {
+  let rules = smartQuotes.concat(ellipsis, emDash),
+    type;
+  if ((type = schema.nodes.indentation)) rules.push(blockQuoteRule(type));
+  if ((type = schema.nodes.ordered_list)) rules.push(orderedListRule(type));
+  if ((type = schema.nodes.bullet_list)) rules.push(bulletListRule(type));
+  if ((type = schema.nodes.code_block)) rules.push(codeBlockRule(type));
+  if ((type = schema.nodes.heading)) rules.push(headingRule(type, 6));
+  return inputRules({ rules });
 }
