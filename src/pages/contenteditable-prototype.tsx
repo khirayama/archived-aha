0248ce425578ea9f1cdf920827911e56c4d98bce
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-12-02 10:52:17 +0900

    Update contenteditable prototype

--- src/pages/contenteditable-prototype.tsx
+++ src/pages/contenteditable-prototype.tsx
@@ -1,45 +1,66 @@
 import * as React from 'react';
+import Head from 'next/head';
 
 import styles from './contenteditable-prototype.module.scss';
 
-export default function ContentEditablePage(props) {
-  const [state, setState] = React.useState(['0000', '1111', '2222', '3333', '4444']);
+function Block(props) {
+  const text = props.text;
   const ref = React.useRef(null);
 
+  const handleTouchStart = React.useCallback((event) => {
+    if (event.target.classList.contains(styles['handle'])) {
+      event.preventDefault();
+    }
+  });
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener('touchstart', handleTouchStart, { passive: false });
+      ref.current.addEventListener('touchmove', handleTouchStart, { passive: false });
+    }
+    return () => {
+      ref.current.removeEventListener('touchstart', handleTouchStart);
+      ref.current.removeEventListener('touchmove', handleTouchStart);
+    };
+  });
+
   return (
-    <ul className={styles['ul']}>
-      {state.map((text, i) => {
-        return (
-          <li
-            key={text}
-            className={styles['li']}
-            draggable
-            onDragStart={(event) => {
-              event.dataTransfer.setData('text/plain', null);
-            }}
-            onDragOver={(event) => {
-              ref.current = i;
-              event.preventDefault();
-            }}
-            onDragEnd={(event) => {
-              console.log(ref.current, i);
-              if (ref.current !== null) {
-                const d = state.concat();
-                const tmp = d[ref.current];
-                d[ref.current] = d[i];
-                d[i] = tmp;
-                console.log(d);
-                setState(d);
-              }
-            }}
-          >
-            HHH
-            <span contentEditable className={styles['text']}>
-              {text}
-            </span>
-          </li>
-        );
-      })}
-    </ul>
+    <li className={styles['li']} ref={ref}>
+      <span
+        className={styles['handle']}
+        onPointerDown={(event) => {
+          event.preventDefault();
+          // event.dataTransfer.setData('text/plain', null);
+          event.target.parentNode.style.opacity = 0.5;
+        }}
+        onPointerOver={(event) => {
+          event.preventDefault();
+          event.target.parentNode.style.opacity = 0.1;
+        }}
+        onPointerUp={(event) => {
+          event.target.parentNode.style.opacity = '';
+        }}
+      >
+        HHH
+      </span>
+      <span contentEditable className={styles['text']} dangerouslySetInnerHTML={{ __html: text }} />
+    </li>
+  );
+}
+
+export default function ContentEditablePage(props) {
+  const [state, setState] = React.useState(['0000', '1111', '2222', '3333', '4444']);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <ul className={styles['ul']}>
+        {state.map((text, i) => {
+          return <Block key={i} text={text} />;
+        })}
+      </ul>
+    </>
   );
 }
