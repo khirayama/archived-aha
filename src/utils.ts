f3f58da003bbb610363e77ae737d44de085dd8f4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-12-26 10:32:30 +0900

    Implement keepSelectionPosition

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,32 @@ export function afterRendering(callback: Function) {
   window.setTimeout(callback, 0);
 }
 
+export function keepSelectionPosition() {
+  const sel = window.getSelection();
+
+  let blockElement = sel.anchorNode.parentElement;
+  while (!blockElement.dataset.blockid) {
+    blockElement = blockElement.parentElement;
+  }
+  const anchorOffset = sel.anchorOffset;
+  const focusOffset = sel.focusOffset;
+
+  afterRendering(() => {
+    const range = document.createRange();
+    const el = blockElement.querySelector('[contenteditable]') as any;
+    if (el.childNodes.length === 0) {
+      const textNode = document.createTextNode('');
+      el.appendChild(textNode);
+    }
+    const textNode = el.childNodes[0];
+    el.focus();
+    range.setStart(textNode, anchorOffset);
+    range.setEnd(textNode, focusOffset);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  });
+}
+
 export function findNextBlock(el): HTMLElement | null {
   const els = [...Array.from(document.querySelectorAll('.' + styles['text']))] as HTMLElement[];
   return (
