61641808fcb11888292b8103c0cde44165fa64ae
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-12-28 06:48:06 +0900

    Create floating nav as experimental

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { Schema, paragraphSchema, listSchema } from '../schema';
 import { Paper } from '../model';
 import { keepSelectionPosition } from '../utils';
 
+import styles from './index.module.scss';
+
 export function getServerSideProps() {
   return {
     props: {
@@ -18,20 +20,26 @@ export function CommandButton(props) {
 
   return (
     <button
-      onMouseDown={() => {
+      onMouseDown={(event) => {
+        event.preventDefault();
         keepSelectionPosition();
         const sel = window.getSelection();
+        if (sel.anchorNode === null) {
+          return;
+        }
         blockElement = sel.anchorNode.parentElement;
-        if (blockElement !== document.body && blockElement.dataset) {
+        if (blockElement && blockElement !== document.body && blockElement.dataset) {
           while (!blockElement.dataset.blockid) {
             blockElement = blockElement.parentElement;
           }
         }
       }}
       onClick={(event) => {
-        const blockId = blockElement.dataset.blockid;
-        const block = props.paper.blocks.filter((b) => b.id === blockId)[0];
-        props.onClick(event, block);
+        if (blockElement && blockElement !== document.body && blockElement.dataset) {
+          const blockId = blockElement.dataset.blockid;
+          const block = props.paper.blocks.filter((b) => b.id === blockId)[0];
+          props.onClick(event, block);
+        }
       }}
     >
       {props.children}
@@ -45,18 +53,67 @@ const paper = new Paper();
 export default function ProtoPage(props) {
   paper.setBlocks(props.blocks);
 
+  React.useEffect(() => {
+    function viewportHandler(event) {
+      requestAnimationFrame(() => {
+        const el = document.querySelector('.' + styles['floating-nav']);
+        if (el && (navigator.platform.indexOf('iOS') !== -1 || navigator.platform.indexOf('iPad') !== -1)) {
+          el.style.bottom = window.innerHeight - window.visualViewport.height + 'px';
+        }
+      });
+    }
+
+    window.visualViewport.addEventListener('scroll', viewportHandler, { passive: true });
+    window.visualViewport.addEventListener('resize', viewportHandler, { passive: true });
+  }, []);
+
   return (
-    <div>
-      <CommandButton
-        schema={schema}
-        paper={paper}
-        onClick={(event, block) => {
-          console.log('Do something', block);
-        }}
-      >
-        IIIII
-      </CommandButton>
+    <div className={styles['container']}>
       <PaperComponent schema={schema} paper={paper} />
+      <div className={styles['floating-nav']}>
+        <CommandButton
+          schema={schema}
+          paper={paper}
+          onClick={(event, block) => {
+            paper
+              .tr(() => {
+                const newBlocks = paper.blocks.map((b) => {
+                  if (b.id === block.id) {
+                    b.indent = Math.min(b.indent + 1, 8);
+                  }
+                  return {
+                    ...b,
+                  };
+                });
+                paper.setBlocks(newBlocks);
+              })
+              .commit();
+          }}
+        >
+          Indent
+        </CommandButton>
+        <CommandButton
+          schema={schema}
+          paper={paper}
+          onClick={(event, block) => {
+            paper
+              .tr(() => {
+                const newBlocks = paper.blocks.map((b) => {
+                  if (b.id === block.id) {
+                    b.indent = Math.max(b.indent - 1, 0);
+                  }
+                  return {
+                    ...b,
+                  };
+                });
+                paper.setBlocks(newBlocks);
+              })
+              .commit();
+          }}
+        >
+          Outdent
+        </CommandButton>
+      </div>
     </div>
   );
 }
