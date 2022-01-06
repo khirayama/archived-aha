19124b9984a22fb435dbe41784153a62115f2e7e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-01-07 07:44:48 +0900

    Create utils file for components

---
+++ src/components/utils.ts
@@ -0,0 +1,33 @@
+export function afterRendering(callback: Function) {
+  window.setTimeout(callback, 0);
+}
+
+export function keepSelectionPosition() {
+  const sel = window.getSelection();
+
+  if (sel.anchorNode === null) {
+    return;
+  }
+
+  let blockElement = sel.anchorNode.parentElement;
+  while (!blockElement.dataset.blockid) {
+    blockElement = blockElement.parentElement;
+  }
+  const anchorOffset = sel.anchorOffset;
+  const focusOffset = sel.focusOffset;
+
+  afterRendering(() => {
+    const range = document.createRange();
+    const el = blockElement.querySelector('[contenteditable]') as any;
+    if (el.childNodes.length === 0) {
+      const textNode = document.createTextNode('');
+      el.appendChild(textNode);
+    }
+    const textNode = el.childNodes[0];
+    el.focus();
+    range.setStart(textNode, anchorOffset);
+    range.setEnd(textNode, focusOffset);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  });
+}
