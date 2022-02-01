805a7c1273d7939ac2f0574cd024ddcfb15d8652
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-02-01 09:34:36 +0900

    Refactor dom-prototype

--- src/dom-prototype/view.ts
+++ src/dom-prototype/view.ts
@@ -1,9 +1,10 @@
+import deepEqual from 'fast-deep-equal';
+
 import { Schema, Block } from './schema';
 import { Paper, Cursor } from './model';
 import { CommandContext, commands } from '../dom-prototype/commands';
 
 export type BlockViewProps<T = Block> = {
-  key: string;
   paper: Paper;
   schema: Schema;
   block: T;
@@ -21,6 +22,10 @@ class BlockView {
     this.render(props);
     this.addEventListeners();
   }
+
+  public mount() {}
+
+  public update() {}
 
   private addEventListeners() {}
 
@@ -42,7 +47,6 @@ class BlockView {
 }
 
 type PaperViewProp = {
-  key: string;
   schema: Schema;
   paper: Paper;
 };
@@ -52,19 +56,108 @@ export class PaperView {
 
   public props: PaperViewProp;
 
-  private _: any = {
-    order: {
-      prev: [],
-      next: [],
-    },
-    keys: [],
-    map: {},
-  };
+  private map: {
+    [blockId: string]: BlockView;
+  } = {};
 
   constructor(props: PaperViewProp) {
     this.props = props;
-    this.render(props);
+    this.mount();
+  }
+
+  private addEventListeners() {
+    const observer = new MutationObserver(() => {
+      this.sync();
+    });
+
+    observer.observe(this.el, {
+      attributes: true,
+      attributeOldValue: true,
+      characterData: true,
+      characterDataOldValue: true,
+      childList: true,
+      subtree: true,
+    });
+
+    this.props.paper.onChange(() => {
+      this.update();
+    });
+
+    this.el.addEventListener('drop', this.onDrop.bind(this));
+    this.el.addEventListener('paste', this.onPaste.bind(this));
+    this.el.addEventListener('keydown', this.onKeyDown.bind(this));
+  }
+
+  private mount() {
+    this.el = document.createElement('div');
+    this.el.contentEditable = 'true';
+    this.props.paper.blocks.forEach((block) => {
+      const props: BlockViewProps = {
+        paper: this.props.paper,
+        schema: this.props.schema,
+        block,
+      };
+      this.createBlock(props);
+      this.el.appendChild(this.map[block.id].el);
+    });
     this.addEventListeners();
+  }
+
+  private update() {
+    this.props.paper.blocks.forEach((block, i) => {
+      const props: BlockViewProps = {
+        paper: this.props.paper,
+        schema: this.props.schema,
+        block,
+      };
+      if (this.map[block.id]) {
+        this.updateBlock(props);
+        this.el.appendChild(this.map[block.id].el);
+      } else {
+        this.createBlock(props);
+        this.el.appendChild(this.map[block.id].el);
+      }
+    });
+  }
+
+  private createBlock(props: BlockViewProps) {
+    const blockView = new BlockView(props);
+    this.map[props.block.id] = blockView;
+  }
+
+  private updateBlock(props: BlockViewProps) {
+    if (this.map[props.block.id]) {
+      this.map[props.block.id].update();
+    }
+  }
+
+  private removeBlock(blockId: string) {
+    if (this.map[blockId]) {
+      this.el.removeChild(this.map[blockId].el);
+      delete this.map[blockId];
+    }
+  }
+
+  private sync() {
+    const newBlocks = [];
+    const els = document.querySelectorAll<HTMLDivElement>('[data-blockid]');
+    for (let i = 0; i < els.length; i += 1) {
+      const el = els[i];
+      const id = el.dataset.blockid;
+      const block = this.props.paper.findBlock(id);
+      if (block) {
+        const schema = this.props.schema.find(block.type);
+        const newBlock = schema.view.toBlock(el);
+        if (newBlock) {
+          newBlocks.push(newBlock);
+        } else {
+          this.removeBlock(id);
+        }
+      } else {
+        this.removeBlock(id);
+      }
+    }
+    this.props.paper.setBlocks(newBlocks);
   }
 
   private getCursor(): Cursor {
@@ -92,167 +185,74 @@ export class PaperView {
     return cursor;
   }
 
-  private addEventListeners() {
-    const observer = new MutationObserver(() => {
-      const newBlocks = [];
-      const els = document.querySelectorAll<HTMLDivElement>('[data-blockid]');
-      for (let i = 0; i < els.length; i += 1) {
-        const el = els[i];
-        const id = el.dataset.blockid;
-        const block = this.props.paper.findBlock(id);
-        if (block) {
-          const schema = this.props.schema.find(block.type);
-          const newBlock = schema.view.toBlock(el);
-          if (newBlock) {
-            newBlocks.push(newBlock);
-          }
-        }
-      }
-      this.props.paper.setBlocks(newBlocks);
-      this.render(this.props);
-    });
+  private onKeyDown(event) {
+    const ctx: CommandContext = {
+      schema: this.props.schema,
+      paper: this.props.paper,
+      cursor: this.getCursor(),
+    };
 
-    observer.observe(this.el, {
-      attributes: true,
-      attributeOldValue: true,
-      characterData: true,
-      characterDataOldValue: true,
-      childList: true,
-      subtree: true,
-    });
+    const key = event.key;
+    // const meta = event.metaKey;
+    const shift = event.shiftKey;
+    const ctrl = event.ctrlKey;
 
-    this.props.paper.onChange(() => {
-      this.render(this.props);
-    });
-
-    this.el.addEventListener('drop', (event) => {
-      event.preventDefault();
-    });
-
-    this.el.addEventListener('paste', (event) => {
-      // TODO toBlock(el)するようにする
-      event.preventDefault();
-    });
-
-    this.el.addEventListener('keydown', (event) => {
-      const ctx: CommandContext = {
-        schema: this.props.schema,
-        paper: this.props.paper,
-        cursor: this.getCursor(),
-      };
-
-      const key = event.key;
-      // const meta = event.metaKey;
-      const shift = event.shiftKey;
-      const ctrl = event.ctrlKey;
-
-      switch (key) {
-        case 'b': {
-          if (ctrl) {
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'i': {
-          if (ctrl) {
-            event.preventDefault();
-          }
-          break;
-        }
-        case 's': {
-          if (ctrl) {
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Enter': {
-          if (ctrl) {
-            console.log('Take action');
-          } else {
-            if (!event.isComposing) {
-              event.preventDefault();
-              commands.splitBlock(ctx);
-              this.props.paper.commit();
-            }
-          }
-          break;
-        }
-        case 'Backspace': {
-          if (this.props.paper.blocks.length === 1 && this.props.paper.blocks[0].text.length === 0) {
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Tab': {
+    switch (key) {
+      case 'b': {
+        if (ctrl) {
           event.preventDefault();
-          if (shift) {
-            commands.outdent(ctx);
-          } else {
-            commands.indent(ctx);
-          }
-          this.props.paper.commit();
-          break;
         }
+        break;
       }
-    });
-  }
-
-  private prepare(props: PaperViewProp) {
-    this.props = props;
-    this._.order.next = [];
-  }
-
-  private create<T extends { key: string }>(view: any, props: T) {
-    this._.order.next.push(props.key);
-    if (this._.map[props.key]) {
-      this._.map[props.key].render(props);
-    } else {
-      const v = new view(props);
-      this._.map[props.key] = v;
-    }
-  }
-
-  private clean() {
-    const keys = Object.keys(this._.map);
-    for (let i = 0; i < this._.keys.length; i += 1) {
-      const key = this._.keys[i];
-      if (keys.indexOf(key) === -1) {
-        const el = this._.map[key].el;
-        this.el.removeChild(el);
-        delete this._.map[key];
+      case 'i': {
+        if (ctrl) {
+          event.preventDefault();
+        }
+        break;
+      }
+      case 's': {
+        if (ctrl) {
+          event.preventDefault();
+        }
+        break;
+      }
+      case 'Enter': {
+        if (ctrl) {
+          console.log('Take action');
+        } else {
+          if (!event.isComposing) {
+            event.preventDefault();
+            commands.splitBlock(ctx);
+            this.props.paper.commit();
+          }
+        }
+        break;
+      }
+      case 'Backspace': {
+        if (this.props.paper.blocks.length === 1 && this.props.paper.blocks[0].text.length === 0) {
+          event.preventDefault();
+        }
+        break;
+      }
+      case 'Tab': {
+        event.preventDefault();
+        if (shift) {
+          commands.outdent(ctx);
+        } else {
+          commands.indent(ctx);
+        }
+        this.props.paper.commit();
+        break;
       }
     }
-    this._.keys = keys;
-
-    const l = Math.max(this._.order.next.length, this._.order.prev.length);
-    for (let i = 0; i < l; i += 1) {
-      const prevKey = this._.order.prev[i];
-      const nextKey = this._.order.next[i];
-      if (prevKey !== nextKey && this._.order.next[i - 1]) {
-        this._.map[this._.order.next[i - 1]].el.insertAdjacentElement('afterend', this._.map[nextKey].el);
-      }
-    }
-    this._.order.prev = this._.order.next;
   }
 
-  private render(props: PaperViewProp) {
-    this.prepare(props);
+  private onPaste() {
+    // TODO toBlock(el)するようにする
+    event.preventDefault();
+  }
 
-    if (this.el === null) {
-      this.el = document.createElement('div');
-      this.el.contentEditable = 'true';
-    }
-
-    this.props.paper.blocks.forEach((block, i) => {
-      const props: BlockViewProps = {
-        key: block.id,
-        paper: this.props.paper,
-        schema: this.props.schema,
-        block,
-      };
-      this.create<BlockViewProps>(BlockView, props);
-    });
-
-    this.clean();
+  private onDrop() {
+    event.preventDefault();
   }
 }
