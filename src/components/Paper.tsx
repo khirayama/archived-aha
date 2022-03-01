5f759aa52ee6b8cc9e6269409d67439b23fc7ea1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-03-02 07:32:21 +0900

    Add TODO

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -526,6 +526,7 @@ export class PaperComponent extends React.Component<PaperComponentProps, PaperCo
                       const e = Math.max(sel.anchorOffset, sel.focusOffset);
                       const defaultSchema = ctx.schema.defaultSchema();
                       const currentSchema = ctx.schema.find(ctx.block.type);
+                      // TODO 最初の文字列は、今いるブロックに結合
                       const newBlock =
                         currentSchema.isContinuation !== false
                           ? ctx.schema.createBlock(currentSchema.type as Block['type'], {
