aa8e981212caa4139ce11a3e6b2ac794eb0b01d8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-03-29 22:19:46 +0900

    Create libs for prosemirror prototype

---
+++ src/libs/schema.ts
@@ -0,0 +1,123 @@
+import { Schema } from 'prosemirror-model';
+import { schema as basicSchema } from 'prosemirror-schema-basic';
+import { InputRule, inputRules } from 'prosemirror-inputrules';
+
+import styles from '../pages/prosemirror.module.scss';
+
+function createBlockNode(nodeSpec) {
+  const attrs = {
+    ...(nodeSpec.attrs || {}),
+    indent: {
+      default: 0,
+    },
+  };
+
+  const originalGetAttrs = nodeSpec.parseDOM[0].getAttrs || (() => ({}));
+  const getAttrs = (dom) => {
+    const indent = Number(dom.getAttribute('indent'));
+    return {
+      ...originalGetAttrs(dom),
+      indent,
+    };
+  };
+  nodeSpec.parseDOM[0].getAttrs = getAttrs;
+
+  const originalToDOM = nodeSpec.toDOM;
+  const toDOM = (node) => {
+    const dom = originalToDOM(node);
+    dom[1].indent = node.attrs.indent;
+    return dom;
+  };
+  nodeSpec.toDOM = toDOM;
+
+  return {
+    ...nodeSpec,
+    group: 'block',
+    content: 'text*',
+    // draggable: false,
+    selectable: true,
+    attrs,
+  };
+}
+
+export const schema = new Schema({
+  nodes: {
+    doc: {
+      content: 'block+',
+    },
+    paragraph: createBlockNode({
+      parseDOM: [
+        {
+          tag: 'p',
+          attrs: {
+            type: 'paragraph',
+          },
+        },
+      ],
+      toDOM: (node) => {
+        return [
+          'p',
+          {
+            type: 'paragraph',
+            class: styles['paragraph'],
+          },
+          [
+            'span',
+            {
+              class: styles['handle'],
+            },
+          ],
+          [
+            'span',
+            {
+              class: styles['indentation'],
+            },
+          ],
+          [
+            'span',
+            {
+              class: styles['text'],
+            },
+            ['span', 0],
+          ],
+        ];
+      },
+    }),
+    quote: createBlockNode({
+      parseDOM: [
+        {
+          tag: 'blockquote',
+          attrs: {
+            type: 'quote',
+          },
+        },
+      ],
+      toDOM: (node) => {
+        return [
+          'blockquote',
+          {
+            type: 'quote',
+            class: styles['quote'],
+          },
+          ['span', 0],
+        ];
+      },
+    }),
+    text: {
+      inline: true,
+    },
+  },
+});
+
+export function buildInputRules() {
+  return inputRules({
+    rules: [
+      new InputRule(/^\s*>\s$/, (state, match, start, end) => {
+        const tr = state.tr.delete(start, end);
+        const node = state.selection.$from.node();
+        tr.setBlockType(start, end, schema.nodes.quote, node.attrs);
+        return tr;
+      }),
+    ],
+  });
+}
