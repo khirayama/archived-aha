2a3a5a86a0574dbb156c1330fe7a61e5c7eb7a2c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-09-04 09:27:32 +0900

    feat: create useUser hooks

--- app/src/services/index.tsx
+++ app/src/services/index.tsx
@@ -1,6 +1,22 @@
 import useSWR from 'swr';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  doc,
+  query,
+  where,
+  addDoc,
+  getDocs,
+  getDoc,
+  documentId,
+  setDoc,
+  onSnapshot,
+} from 'firebase/firestore';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const db = getFirestore();
 
 /* [はじめに – SWR](https://swr.vercel.app/ja/docs/getting-started) */
 function useUser(id) {
@@ -11,4 +27,41 @@ function useUser(id) {
     isLoading: !error && !data,
     isError: error,
   };
+}
+
+type User = {
+  email: string;
+  profile: {
+    username: string;
+  };
+};
+
+function useUser(): User {
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+  if (auth) {
+    getDoc(doc(db, 'profiles', auth.currentUser.uid)).then((res) => {
+      const profile = res.data();
+      setUser({
+        email: auth.currentUser.email,
+        profile,
+      });
+    });
+  }
+
+  onAuthStateChanged(auth, (u) => {
+    const auth = getAuth();
+    if (auth) {
+      getDoc(doc(db, 'profiles', auth.currentUser.uid)).then((res) => {
+        const profile = res.data();
+        setUser({
+          email: auth.currentUser.email,
+          profile,
+        });
+      });
+    }
+  });
+
+  return user;
 }
