e520ce756de1aa54d592c31eab1f60a65c6a546c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-09-19 16:19:37 +0900

    feat: support new propaties for design system

--- app/src/design-system/utils.ts
+++ app/src/design-system/utils.ts
@@ -2,17 +2,22 @@ const theme = {
   space: 4,
 };
 
-function mapStyle(props, style, propsKeys: string[], mappedKeys: string[], unit: string = 'px') {
+function mapStyle(props, style, propsKeys: string[], mappedKeys: string[], fn?: Function) {
   for (let i = 0; i < propsKeys.length; i += 1) {
     const propsKey = propsKeys[i];
-    if (props[propsKey] !== undefined) {
+    const v = props[propsKey];
+    if (v !== undefined) {
       for (let j = 0; j < mappedKeys.length; j += 1) {
         const mappedKey = mappedKeys[j];
-        style[mappedKey] = theme.space * props[propsKey] + unit;
+        style[mappedKey] = fn ? fn(v) : v;
       }
       delete props[propsKey];
     }
   }
+}
+
+function s2p(v) {
+  return theme.space * v + 'px';
 }
 
 function designSysytemProps(props: {
@@ -35,20 +40,29 @@ function designSysytemProps(props: {
   paddingStart?: number;
   pe?: number;
   paddingEnd?: number;
+  /* flex */
+  flex?: number;
+  /* width */
+  w?: string;
+  width?: string;
 }) {
   const newProps = { ...props };
 
   const style = {};
   /* padding */
-  mapStyle(newProps, style, ['p', 'padding'], ['padding']);
-  mapStyle(newProps, style, ['pt', 'paddingTop'], ['paddingTop']);
-  mapStyle(newProps, style, ['pb', 'paddingBottom'], ['paddingBottom']);
-  mapStyle(newProps, style, ['pl', 'paddingLeft'], ['paddingLeft']);
-  mapStyle(newProps, style, ['pr', 'paddingRight'], ['paddingRight']);
-  mapStyle(newProps, style, ['px', 'paddingX'], ['paddingLeft', 'paddingRight']);
-  mapStyle(newProps, style, ['py', 'paddingY'], ['paddingTop', 'paddingBottom']);
-  mapStyle(newProps, style, ['ps', 'paddingStart'], ['paddingInlineStart']);
-  mapStyle(newProps, style, ['pe', 'paddingEnd'], ['paddingInlineEnd']);
+  mapStyle(newProps, style, ['p', 'padding'], ['padding'], s2p);
+  mapStyle(newProps, style, ['pt', 'paddingTop'], ['paddingTop'], s2p);
+  mapStyle(newProps, style, ['pb', 'paddingBottom'], ['paddingBottom'], s2p);
+  mapStyle(newProps, style, ['pl', 'paddingLeft'], ['paddingLeft'], s2p);
+  mapStyle(newProps, style, ['pr', 'paddingRight'], ['paddingRight'], s2p);
+  mapStyle(newProps, style, ['px', 'paddingX'], ['paddingLeft', 'paddingRight'], s2p);
+  mapStyle(newProps, style, ['py', 'paddingY'], ['paddingTop', 'paddingBottom'], s2p);
+  mapStyle(newProps, style, ['ps', 'paddingStart'], ['paddingInlineStart'], s2p);
+  mapStyle(newProps, style, ['pe', 'paddingEnd'], ['paddingInlineEnd'], s2p);
+  /* flex */
+  mapStyle(newProps, style, ['flex'], ['flex']);
+  /* width */
+  mapStyle(newProps, style, ['w', 'width'], ['width']);
 
   newProps.style = { ...newProps.style, ...style };
   return newProps;
