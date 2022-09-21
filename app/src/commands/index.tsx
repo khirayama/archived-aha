097eed4d4e9881377186eb656094644c67613323
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-09-21 21:50:15 +0900

    feat: implement signUp, signIn, signOut functions

--- app/src/commands/index.tsx
+++ app/src/commands/index.tsx
@@ -3,7 +3,49 @@ import { getFirestore, doc, collection, setDoc, addDoc } from 'firebase/firestor
 
 const db = getFirestore();
 
-export function signOut(auth) {
-  const auth = getAuth();
-  auth.signOut();
+export function signUp(email: string, password: string, username: string) {
+  return new Promise((resolve) => {
+    const auth = getAuth();
+    if (auth) {
+      createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const u = userCredential.user;
+        setDoc(doc(db, 'profiles', u.uid), { username });
+        addDoc(collection(db, 'papers'), {
+          uid: user.uid,
+          tags: [],
+          blocks: [schema.createBlock('heading', { text: '', attrs: { level: 1 } })],
+        }).then((paperRef) => {
+          Promise.all([
+            setDoc(doc(db, 'arrangements', u.uid), {
+              front: [paperRef.id],
+              archived: [],
+            }),
+            setDoc(doc(db, 'ownerships', paperRef.id), { [u.uid]: 'admin' }),
+          ]).then(() => {
+            resolve();
+          });
+        });
+      });
+    }
+  });
+}
+
+export function signIn(email: string, password: string) {
+  return new Promise((resolve) => {
+    const auth = getAuth();
+    if (auth) {
+      signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        resolve();
+      });
+    }
+  });
+}
+
+export function signOut() {
+  return new Promise((resolve) => {
+    const auth = getAuth();
+    if (auth) {
+      auth.signOut().then(resolve);
+    }
+  });
 }
