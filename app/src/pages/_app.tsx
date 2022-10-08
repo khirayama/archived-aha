5fc87e673736b1917b91b478b4789214751c8f3d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-10-08 20:42:34 +0900

    feat: update _app

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import Head from 'next/head';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+import { init as initI18n } from '../i18n';
 
 import './reset.scss';
 import './theme.scss';
+
+initI18n();
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function App({ Component, pageProps }) {
   return (
