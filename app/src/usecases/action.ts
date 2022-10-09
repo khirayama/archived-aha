4ef33e15e2a264d41de9c9415d15e584be977306
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-10-09 16:48:13 +0900

    feat: implement createPaper function

---
+++ app/src/usecases/action.ts
@@ -0,0 +1,44 @@
+import { schema } from '../components/Editor';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, doc, addDoc, setDoc, getDoc } from 'firebase/firestore';
+
+const db = getFirestore();
+
+export function createPaper() {
+  return new Promise((resolve) => {
+    const auth = getAuth();
+
+    if (auth.currentUser) {
+      const uid = auth.currentUser.uid;
+
+      Promise.all([
+        getDoc(doc(db, 'arrangements', uid)),
+        addDoc(collection(db, 'papers'), {
+          uid,
+          tags: [],
+          blocks: [schema.createBlock('heading', { text: '', attrs: { level: 1 } })],
+        }),
+      ])
+        .then((res) => {
+          const arrangement = res[0].data();
+          const paperRef = res[1];
+
+          const front = arrangement.front.concat();
+          front.push(paperRef.id);
+
+          Promise.all([
+            setDoc(doc(db, 'arrangements', uid), {
+              ...arrangement,
+              front,
+            }),
+            setDoc(doc(db, 'ownerships', paperRef.id), { [uid]: 'admin' }),
+            setDoc(doc(db, 'accesses', paperRef.id), {
+              target: 'private',
+              role: 'none',
+            }),
+          ]);
+        })
+        .then(resolve);
+    }
+  });
+}
