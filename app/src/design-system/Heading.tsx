96d7cae38113dca3897827b84b20f015b1110120
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-10-11 17:51:23 +0900

    feat: support as props to Heading component

--- app/src/design-system/Heading.tsx
+++ app/src/design-system/Heading.tsx
@@ -1,6 +1,12 @@
+import { createElement } from 'react';
+
 import { dsp } from './utils';
 
+/* TODO: Support size
+ * https://chakra-ui.com/docs/components/heading
+ */
 export function Heading(props) {
   const p = dsp(props);
-  return <h1 {...p} />;
+  const tag = props.as || 'h1';
+  return createElement(tag, p);
 }
