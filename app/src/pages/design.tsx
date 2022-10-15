7ce517fac565b675648038fda3a1046428bfe8ab
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-10-15 15:41:18 +0900

    feat: create design page

---
+++ app/src/pages/design.tsx
@@ -0,0 +1,86 @@
+import { renderToStaticMarkup } from 'react-dom/server';
+import prettify from 'html-prettify';
+
+import {
+  /* Layout */
+  Box,
+  Flex,
+  /* Form */
+  FormControl,
+  Input,
+  Button,
+  /* Navigation */
+  Link,
+  /* Typography */
+  Text,
+  Heading,
+  /* Display */
+  List,
+  ListItem,
+} from '../design-system';
+import { dsp } from '../design-system/utils';
+
+function Code(props) {
+  const p = dsp(props);
+  return <code {...p} />;
+}
+
+function Preview(props) {
+  return (
+    <Box>
+      <Code style={{ whiteSpace: 'pre' }}>{prettify(renderToStaticMarkup(props.children).replace(/></g, '>\n<'))}</Code>
+      <Box>{props.children}</Box>
+    </Box>
+  );
+}
+
+export default function DesignPage() {
+  return (
+    <Box>
+      <Box p={4}>
+        <Heading>Design System</Heading>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Properties</Heading>
+        <Text>padding, flex, width</Text>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Layout</Heading>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Form</Heading>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Navigation</Heading>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Typography</Heading>
+
+        <Box p={4}>
+          <Heading as="h3">Heading</Heading>
+          <Box>
+            <Text>props: as</Text>
+            <Text>scheduled: size</Text>
+          </Box>
+          <Preview>
+            <Heading>Heading 1</Heading>
+            <Heading as="h2">Heading 2</Heading>
+            <Heading as="h3">Heading 3</Heading>
+            <Heading as="h4">Heading 4</Heading>
+            <Heading as="h5">Heading 5</Heading>
+            <Heading as="h6">Heading 6</Heading>
+          </Preview>
+        </Box>
+      </Box>
+
+      <Box p={4}>
+        <Heading as="h2">Display</Heading>
+      </Box>
+    </Box>
+  );
+}
