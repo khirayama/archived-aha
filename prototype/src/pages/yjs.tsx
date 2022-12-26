bd785498a860bb5efab5311cbec14a8be07a1765
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-12-26 12:21:33 +0900

    feat: use yjs as prototype

--- prototype/src/pages/yjs.tsx
+++ prototype/src/pages/yjs.tsx
@@ -1,3 +1,42 @@
+import * as Y from 'yjs';
+
+const doc1 = new Y.Doc();
+const doc2 = new Y.Doc();
+
+doc1.on('update', (update) => {
+  Y.applyUpdate(doc2, update);
+});
+
+doc2.on('update', (update) => {
+  Y.applyUpdate(doc1, update);
+});
+
+doc1.getArray('myarray').insert(0, ['Hello doc2, you got this?']);
+doc2.getArray('myarray').get(0);
+console.log(doc1.toJSON(), doc2.toJSON());
+
+type Page = {
+  title: string;
+  sections: Section[];
+};
+
+type Section = {
+  heading: string;
+  items: Item[];
+};
+
+type Item = {
+  text: string;
+  indent: number;
+};
+
+const page = new Y.Doc();
+page.getText('title').insert(0, 'THIS IS TITLE');
+const section0 = new Y.Map();
+section0.set('heading', new Y.Text('THIS IS HEADING OF SECTION 0'));
+page.getArray('sections').insert(0, [section0]);
+console.log(page.toJSON());
+
 export default function YjsPage() {
   return (
     <>
